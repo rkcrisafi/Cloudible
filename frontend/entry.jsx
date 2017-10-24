09c3bf2117cb623bfd844e85dbb02cc143ba811f
Regina Kristina Crisafi <kristina@Reginas-MacBook-Pro.local>
2017-10-24 12:32:55 -0400

    Start frontend auth

---
+++ frontend/entry.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { signup, login, logout } from './actions/session_actions';
+// import Root from '.components/root';
+// import configureStore from '.store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<div>Welcome to Cloudible</div>,
+  document.getElementById('root'));
+});
