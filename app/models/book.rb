b1d7c6cca92ee541ffdbab1d06d14a2a83a30dd7
Regina Kristina Crisafi <kristina@Reginas-MacBook-Pro.local>
2017-10-26 16:19:27 -0400

    Change book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,15 +1,15 @@
 class Book < ApplicationRecord
 
-  validates :title, :image, :author, :narrator, :publisher, :length, :unabridged, :language, :summary, presence: true
-
-
-  has_attached_file :image, default_url: "love-books.jpg"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
-  has_many :libraries
-
-  has_many :listeners,
-    through: :libraries,
-    source: :listener
+  # validates :title, :image, :author, :narrator, :publisher, :length, :unabridged, :language, :summary, presence: true
+  #
+  #
+  # has_attached_file :image, default_url: "love-books.jpg"
+  # validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  #
+  # has_many :libraries
+  #
+  # has_many :listeners,
+  #   through: :libraries,
+  #   source: :listener
 
 end
