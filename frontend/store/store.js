e82dc4eebec860bb6cdcad7ac361d0990e79ff95
Regina Kristina Crisafi <rkcrisafi@gmail.com>
2017-11-07 22:14:31 -0500

    Fix some webpack issues

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,11 +2,20 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
 
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    applyMiddleware(...middlewares)
   )
 )
 
